7a3782acac3e0c77186e97e3626086b4428eddfb
wangtian5329 <46984640+wangtian5329@users.noreply.github.com>
2019-02-11 09:03:14 -0600

    Update Army.java

--- src/models/Army.java
+++ src/models/Army.java
@@ -1,17 +1,44 @@
 package models;
 
 public class Army {
+	
+	private Player player;
+	
+	private Country country;
+	
+	/**Creates a new army.*/
+	public Army(Player player) {
+		
+		this.player = player;
+	}
 
-    public Army(Player player){
-
-    }
-
-    public Country getCountry(){
-        Country country = new Country("333");
-        return country;
-    }
-
-    public void setCountry(Country country){
-
-    }
+	/**
+	 * Gets the player.
+	 */
+	public Player getPlayer() {
+		return player;
+	}
+	
+	/**
+	 * Sets the player.
+	 */
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	/**
+	 * Gets the country.
+	 */
+	public Country getCountry() {
+		return country;
+	}
+	
+	/**
+	 * Sets the country.
+	 */
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	
+	
 }

74202a051829027b8e13bd523a62a2973d7846ca
qingli <liqing51269@gamil.com>
2019-02-11 18:02:59 -0500

    Map.java has been changed: add map validation

--- src/models/Map.java
+++ src/models/Map.java
@@ -170,7 +170,7 @@ public class Map {  //
 
         try{
             br = new BufferedReader(new FileReader(nameOfMapFile));
-            //再增加一些读取文件的判断
+            //鍐嶅鍔犱竴浜涜鍙栨枃浠剁殑鍒ゆ柇
             while ((inputLine = br.readLine()) != null){
                 rowNum++ ;
                 inputLine = inputLine.trim();
@@ -253,13 +253,13 @@ public class Map {  //
             }
 
         }catch (IOException e){
-            System.out.println("Loading error："+e);
+            System.out.println("Loading error锛�"+e);
         }
         finally {
             try{
                 if (br != null) br.close();
             }catch (Exception e){
-                System.out.println("Loading error："+e);
+                System.out.println("Loading error锛�"+e);
             }
         }
     }
@@ -268,6 +268,138 @@ public class Map {  //
     public void save_as(){
 
     }
+    
+    
+    /**
+	 * This is the function of Map Validation
+	 * @author QingLi
+	 */
+	
+	
+	/**
+	 * This method is used to check the validation of the map
+	 * 
+	 * @param continents is the map of all continents
+	 * @param connectionMap is the map of all countries and their contiguous countries
+	 * @return true if  map is connected; false if the map has some errors
+	 */
+	public void checkMapValidation() {
+		
+		String signal = isContinentInternallyConnected();
+		if( signal == "ALL PASSED") 
+		{
+			if(isWholeMapConnected()) 
+			{
+				System.out.println("Map is successfully connected!");
+			}
+			else {
+				System.out.println("Error: Some continent are not externally connected");
+			}
+		}
+		else
+		{
+			System.out.println("Error: "+signal+" continent is not connected");
+		}
+	}
+	
+	
+	/**
+	 * This method is used to check continent validation
+	 * 
+	 * @param continents is the map of all continents 
+	 * @return specific continent's name if there is a continent unconnected 
+	 * @return String "ALL PASSED" if all continents are internally connected
+	 */
+	public String isContinentInternallyConnected() {
+		
+		for(String continentname : continents.keySet()) 
+		{
+			//currentctn is the current continent
+			Continent currentctn = continents.get(continentname);
+			//To check the connection of countries of each continent 
+			for(Country ctry1 : currentctn.getCountryList()) 
+			{
+				//Use a integer to count the number of contiguous countries of each country
+				int counter = 0;
+				for(Country ctry2 : currentctn.getCountryList()) 
+				{
+					if(connectionMap.get(ctry1.getName()).contains(ctry2)) 
+					{
+						counter++;
+					}
+				}
+				//Counter==0 means there is a isolate country, so continent is not connected internally,
+				//Return the name of this unconnected continent
+				if(counter == 0) 
+				{
+					return continentname;
+				}
+			}
+		}
+		return "ALL PASSED";
+	}
+	
+	
+	/**
+	 * This method is used to check the validation of the whole map
+	 * 
+	 * @param connectionMap is the map of all countries and their contiguous countries
+	 * @param countries is the map of all countries
+	 * @return true if the whole map is successfully connected; false if it is not connected
+	 */
+	public boolean isWholeMapConnected() {
+		//The first country we used to start doing DFS
+		Country sourceNode = null;
+		
+		//flag is the attribute of the country class, if one country has been searched, the flag is true, otherwise the flag is false
+		//At first we set all flags of countries false
+		for(String ctryname : connectionMap.keySet()) 
+		{
+			Country ctry3 = countries.get(ctryname);
+			ctry3.setflag(false);
+			if(sourceNode == null) 
+			{
+				sourceNode = ctry3;
+			}
+		}
+		
+		//Doing DFS to search all the accessible path of the whole map
+		DFS(sourceNode);
+		
+		//To check if the whole map is connected
+		//If there is a flag of a country is false, means this country is isolated, then reject the map
+		//Otherwise the map is successfully connected
+		for(String ctryname2 : connectionMap.keySet()) 
+		{
+			Country ctry4 = countries.get(ctryname2);
+			if(!ctry4.getflag()) 
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * This method is used to apply DFS algorithm on map validation checking
+	 * @param connectionMap is the map of all countries and their contiguous countries
+	 * @param countries is the map of all countries
+	 * @param sourceNode is the key country we used to check its next connected country 
+	 */
+	public void DFS(Country sourceNode) {
+		sourceNode.setflag(true);
+		//currentctryname is current country name
+		for(String currentctryname : connectionMap.get(sourceNode.getName())) 
+		{
+			//targetNode is the current contiguous country of sourceNode
+			Country targetNode = countries.get(currentctryname);
+			if(!targetNode.getflag()) 
+			{
+				DFS(targetNode);
+			}
+		}
+	}
 
     public static void main(String[] args){
         String fileName = "World.map";
@@ -276,7 +408,10 @@ public class Map {  //
 
         System.out.println( " getcountriesMap-----"+getMapInstance().getCountriesMap());
         System.out.println( "getContinentMap-------"+getMapInstance().getContinentMap());
-        System.out.println( "getConnectionMap-------"+getMapInstance().getConnectionMap());
+        System.out.println( "getConnectionMap-------"+getMapInstance().getConnectionMap());  
+        
+        map.checkMapValidation();
+        
 
     }
 }

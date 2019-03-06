f39e759580c7f85bacf98217afc2123cce8b889a
qingli <liqing51269@gmail.com>
2019-03-06 12:59:20 -0500

    add popups

---
+++ src/Popups/aPopupMenu.java
@@ -0,0 +1,18 @@
+package Popups;
+
+import java.awt.Color;
+
+import View.Theme;
+
+/**
+ * This is the class used to define popup menu
+ *
+ */
+public class aPopupMenu {
+	protected Color MENU_COLOR = Theme.color;
+
+	public aPopupMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

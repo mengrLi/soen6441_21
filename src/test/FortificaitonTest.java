ac0af3c85d95a9da9582051b6a2917a0d8e32fe5
hengyangan <277265318@qq.com>
2019-03-26 00:10:04 -0400

    update fortification test four

--- src/test/FortificaitonTest.java
+++ src/test/FortificaitonTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 /**
  * This is the class used to test fortificaiton class
+ * 
  * @author anhen
  *
  */
@@ -26,36 +27,91 @@ public class FortificaitonTest {
 	public void setEnviorment() {
 		game.setPlayerList(1);
 	}
+
 	@Test
 	public void errorOne() {
 		originalCountry.setName("original country");
 		destCountry.setName("destination country");
 		String message = "Error: original country is not your country, you can not change the number of army of this country!";
-		ArrayList <Country> countryList = new ArrayList<Country>();
+		ArrayList<Country> countryList = new ArrayList<Country>();
 		countryList.add(destCountry);
 		curPlayer.setCountriesOwned(countryList);
 		String string = game.checkMoveArmyTest(armyMoved, curPlayer, destCountry, originalCountry);
 		assertEquals(message, string);
 	}
+
 	@Test
 	public void errorTwo() {
 		originalCountry.setName("original country");
 		destCountry.setName("destination country");
 		String message = "Error: destination country is not your country, you are not able to move army to this country!";
-		ArrayList <Country> countryList = new ArrayList<Country>();
+		ArrayList<Country> countryList = new ArrayList<Country>();
 		countryList.add(originalCountry);
 		curPlayer.setCountriesOwned(countryList);
 		String string = game.checkMoveArmyTest(armyMoved, curPlayer, destCountry, originalCountry);
 		assertEquals(message, string);
 	}
+
 	@Test
 	public void errorThree() {
+		originalCountry.setPlayer(curPlayer);
+		destCountry.setPlayer(curPlayer);
+		originalCountry.increaseArmy();
+		destCountry.increaseArmy();
 		originalCountry.setName("original country");
 		destCountry.setName("destination country");
-		ArrayList <Country> countryList = new ArrayList<Country>();
+		ArrayList<Country> countryList = new ArrayList<Country>();
 		countryList.add(originalCountry);
 		countryList.add(destCountry);
 		curPlayer.setCountriesOwned(countryList);
-		
+		String message = "Error:  There is only 1 army in this country(" + originalCountry.getName()
+				+ "), you can not move it!";
+		String string = game.checkMoveArmyTest(armyMoved, curPlayer, destCountry, originalCountry);
+		assertEquals(message, string);
+	}
+
+	@Test
+	public void errorFour() {
+		originalCountry.setPlayer(curPlayer);
+		destCountry.setPlayer(curPlayer);
+		originalCountry.increaseArmy();
+		originalCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		destCountry.increaseArmy();
+		originalCountry.setName("original country");
+		destCountry.setName("destination country");
+		ArrayList<Country> countryList = new ArrayList<Country>();
+		countryList.add(originalCountry);
+		countryList.add(destCountry);
+		curPlayer.setCountriesOwned(countryList);
+		String message = "Error: There are too many armies in this country(" + destCountry.getName() + "),";
+		String string = game.checkMoveArmyTest(armyMoved, curPlayer, destCountry, originalCountry);
+		assertEquals(message, string);
 	}
 }

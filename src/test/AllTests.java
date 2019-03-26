00be709052b1c4547bc2a57bd3dd341aadcb8344
hengyangan <277265318@qq.com>
2019-03-26 18:54:25 -0400

    Update the test suite

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -4,9 +4,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * @author anhen
+ * This is the test suite for all tests
+ */
 @RunWith(Suite.class)
-@SuiteClasses({ ArmyTest.class, ContinentTest.class, CountryTest.class, CountryTest2.class, PlayerTest.class,
-CardTests.class, GameEngineTests.class, LoadMapTest.class, MapValidationTest.class, SetPlayersTest.class})
+@SuiteClasses({ ArmyTest.class, AttackerDefenderValidation.class, CardForArmies1Test.class, CardForArmies2Test.class,
+		CardTests.class, ContinentTest.class, CountryTest.class, CountryTest2.class, DiceAllTest.class,
+		DiceOneTest.class, DiceThreeTest.class, DiceTwoTest.class, EndOfGameTest.class, FortificaitonTest.class,
+		GameEngineTests.class, LoadMapTest.class, MapValidationTest.class, MoveAfterConqueringTest.class,
+		PlayerEngineTests.class, PlayerTest.class, SetPlayersTest.class })
+
 public class AllTests {
 
 }

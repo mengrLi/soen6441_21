9eb2288ec0db77ec3f16e04b4dfd83fc3c32b446
hengyangan <277265318@qq.com>
2019-04-02 16:07:36 -0400

    update random strategy player

--- src/GameModel/StrategyPlayer/Random.java
+++ src/GameModel/StrategyPlayer/Random.java
@@ -1,32 +1,89 @@
 package GameModel.StrategyPlayer;
 
 import GameModel.Player;
+
+import java.util.ArrayList;
+import java.util.*;
+import GameModel.Country;
+import GameModel.GameState;
 import GameModel.PlayerEngine;
+import MapModel.Map;
 
 /**
- * Created by liaoxiaoyun on 2019-03-30.
+ * This is the class used to implement the random strategy
+ * @author anhen
  */
-public class Random implements Strategy{
-    private PlayerEngine playerEngine;
+public class Random implements Strategy {
+	public String strategyName = "Random";
+	private PlayerEngine playerEngine;
+	private Map map = Map.getMapInstance();
+	public GameState state;
+	public Random() {
+		playerEngine = new PlayerEngine();
+	}
 
-    Random(){
-        playerEngine = new PlayerEngine();
-    }
+	public String getStringName() {
+		return this.strategyName;
+	}
 
+	public void autoReinforce(Player curPlayer) {
+		if (state == GameState.REINFORCE) {
+			ArrayList<Country> randomCountry = curPlayer.getCountriesOwned();
+			int randomNum;
+			int reinforceNum = curPlayer.getNumberOfArmy();
+			randomNum = (int) (Math.random()*curPlayer.getCountriesOwned().size());
+			for (int i = 1; i<= reinforceNum;i++ ) {
+				Country c = randomCountry.get(randomNum-1);
+				c.AddArmy();
+			}
+		}
+	};
 
-    public void autoReinforce(Player curPlayer){
+	public boolean autoAttack(Player curPlayer) {
+		if(state == GameState.ATTACK) {
+			Country curCountry;
+			ArrayList<Country> randomCountry = curPlayer.getCountriesOwned();
+			int randCountryNum = (int)(Math.random()*curPlayer.getCountriesOwned().size());
+			curCountry = randomCountry.get(randCountryNum);
+			ArrayList<Country> contigunousCountry = curCountry.getcontiguousCountryList();
+//			int attackedRandCountrynum = 
+		}
+		
+		boolean ifWinned = false;
 
-    };
+		return ifWinned;
+	};
 
-
-    public boolean autoAttack(Player curPlayer){
-        boolean ifWinned = false;
-
-        return  ifWinned;
-    };
-
-
-    public void autoFortify(Player curPlayer){
-
-    };
+	public void autoFortify(Player curPlayer) {
+		if(state == GameState.FORTIFY) {
+			ArrayList<Country> randCountry = curPlayer.getCountriesOwned();
+			int randCountryNum = (int)(Math.random()*curPlayer.getCountriesOwned().size());
+			Country originalCountry = randCountry.get(randCountryNum);
+			Country destCountry = randCountry.get(randCountryNum);
+			while(originalCountry == destCountry) {
+				destCountry = randCountry.get(randCountryNum);
+			}
+			int moveArmy = (int)(Math.random()*originalCountry.getArmiesNum());
+			playerEngine.moveArmyBetweenCountries(moveArmy, curPlayer, destCountry, originalCountry);
+		}
+	};
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

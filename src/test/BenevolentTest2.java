9b91479d7d8f35ba699100a5f588bcf174f521cb
wangtian5329 <46984640+wangtian5329@users.noreply.github.com>
2019-04-08 20:56:39 -0500

    Create BenevolentTest2.java

---
+++ src/test/BenevolentTest2.java
@@ -0,0 +1,67 @@
+package test;
+import GameModel.*;
+import MapModel.*;
+import GameView.*;
+import MapView.*;
+import GameModel.StrategyPlayer.*;
+import GameController.*;
+import MapController.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Assert.*;
+import org.junit.Before;
+
+public class BenevolentTest2 {
+	
+	PlayerEngine game= new PlayerEngine();
+	Player benevolent=new Player(1);
+	Continent con=new Continent("1",1);
+	Strategy str=new Benevolent();
+	Country c1= new Country();
+	Country c2= new Country();
+	
+	/**
+	 * Set up the initial state
+	 */
+	@Before
+	public void setUp() {
+		ArrayList<Country> countriesOwned=new ArrayList<>();
+		game.setPlayerList(1);
+		benevolent= game.getPlayerList().get(0);
+		con.setPlayer(benevolent);
+		c1.setPlayer(benevolent);
+		c2.setPlayer(benevolent);
+		benevolent.setArmyList(8);
+		for(int i=0;i<3;i++){
+			c1.AddArmy();	
+		}
+		for(int i=0;i<5;i++){
+			c2.AddArmy();
+		}
+		countriesOwned.add(c1);
+		countriesOwned.add(c2);
+		c1.setContinent(con);
+		c2.setContinent(con);
+		benevolent.setCountriesOwned(countriesOwned);
+		con.setCountryList(countriesOwned);
+		con.setValue(3);
+	}
+	/**
+	 * To test if the attack country is belonged to attacker/current player
+	 */
+	@Test
+	public void autoReinforceTest() {
+		benevolent.setStrategy(str);
+		benevolent.autoAttack();
+		// do not attack,so the number of armies are not change
+		assertEquals(3,c1.getArmiesNum());
+		assertEquals(5,c2.getArmiesNum());
+
+	}
+
+}

dce3637398e26e9c6e18dfbbf8c7b1c49415d3a9
qingli <liqing51269@gmail.com>
2019-04-09 20:43:43 -0400

    update testTournament

--- src/GameController/testTournament.java
+++ src/GameController/testTournament.java
@@ -14,33 +14,6 @@ import java.util.Iterator;
  */
 public class testTournament
 {
-    public static void main(String[] args){
-        int cc = 0;
-        ArrayList<Integer> aa = new ArrayList<>();
-        aa.add(cc);
-
-        ArrayList<Integer> bb = aa;
-
-        aa.add(3);
-
-        System.out.println(bb);
-
-        Color ss = Color.black;
-
-        int  name = ss.getRGB();
-        System.out.println(name);
-
-        GameState state = GameState.END;
-       System.out.println(state.name());
-
-       String player = "0,Cheater0,,Country5 Country2,4,Cheater";
-
-       String[] playerInfo = player.split(",");
-       System.out.println(playerInfo.length);
-        System.out.println("playerInfo[2] :"+playerInfo[2]);
-
-        System.out.println(Boolean.parseBoolean("true"));
-    }
 
 
     public static GameState state;//Game State
@@ -64,7 +37,7 @@ public class testTournament
     public static String curMap = "";
     public static ArrayList<String> tournamentResult = new ArrayList<>();
 
-    testTournament(GameState state,ArrayList<Player> playerList,int gameNum,int maxRoundNum,ArrayList<String> mapList){
+    public testTournament(GameState state,ArrayList<Player> playerList,int gameNum,int maxRoundNum,ArrayList<String> mapList){
         this.state = state;
         this.playerList = playerList;
         this.gameNum = gameNum;
